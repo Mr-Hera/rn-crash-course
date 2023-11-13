d8cfe28348fcc24e6f9c55c7b3bb364d9ae24d85
Mr-Hera <randellhera@gmail.com>
2023-11-13 04:41:12 +0300

    explored dynamic display of stored data

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     // console.log(`🚀 ~ Entered text is '${enteredText}'`);
@@ -11,7 +12,10 @@ export default function App() {
   
   function addGoalHandler() {
     console.log(`🚀 ~ Entered goal is '${enteredGoalText}'`);
-    // console.log(enteredGoalText);
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals, 
+      enteredGoalText
+    ]);
   };
 
   return (
@@ -25,7 +29,7 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals: [ ... ]</Text>
+        {courseGoals.map(goal => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
